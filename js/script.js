dbd914a19db43fb76813dc90002be38d467c14dc
Vlad <nesveldinv@gmail.com>
2021-10-13 16:33:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert("Hello")
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "")
+//console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt("Your name?", "");
+answers[1] = prompt("Your last name?", "");
+answers[2] = prompt("Your age?", "");
+
+
